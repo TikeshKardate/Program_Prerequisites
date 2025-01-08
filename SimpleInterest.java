5f03348b6fe84ca7d6ac94522023f0653080748c
TikeshKardate <tikesh.kardate@gmail.com>
2025-01-08 16:52:27 +0530

    Added Simple Interest Program Day01

---
+++ SimpleInterest.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class SimpleInterest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Principle:"); 
+		
+		double p = sc.nextDouble(); 
+		
+		 System.out.println("Rate:"); 
+		
+		double r = sc.nextDouble(); 
+		
+		 System.out.println("Time:"); 
+		
+		double t = sc.nextDouble(); 
+	
+		double si = ((p*r*t)/100); 
+		
+		System.out.println("Simple Interest: " + si);
+    }
+}
